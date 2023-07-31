ef98598a2789d812d6886dd876b80d95ccfde900
Berkowitz <55586@icf.com>
2023-07-31 14:58:49 -0400

    added some planning notes for next steps

--- modules/multipathway_processing.py
+++ modules/multipathway_processing.py
@@ -1,8 +1,12 @@
+import pandas as pd
+
 """
-This occurs across a series of Access queries. 
-These queries must use only the sources inside the source category, 
-and they must use the emissions after any speciations were applied 
-in the basic processing noted above.
+TODO
+- move overrides out
+- split into
+    - latitudes/longitudes
+    - HH
+    - Eco
 
 ONLY run this on actual/allowable emissions, NOT acute
 
@@ -21,6 +25,8 @@ class MultiPathwayProcessing:
         self.qryMP03a_SourceLatLons_CatLevel()
         self.qryMP03aa_LineLatLons01()
         self.qryMP03aa_LineLatLons02()
+        self.qryMP03ab_AppendLineLatLongs() # not implemented
+        self.qryMP04aHH_CreateShellForChemSVs()
         return self.df
 
     # working_CrosswalkEmissionInventory_Eco
@@ -37,70 +43,134 @@ class MultiPathwayProcessing:
 
     # working_MPLatLongs
     def qryMP03a_SourceLatLons_CatLevel(self):
-        columns = [
-            "ICFFacilityID", # Facility ID
-            "sppd_facility_identifier",
-            "ICFSourceID", # Source ID
-            "y_coordinate", # Latitude
-            "x_coordinate", # Longitude
-            "ICFCatLevelModeling",
-        ]
+        columns = {
+            "ICFFacilityID" : "Facility ID",
+            "sppd_facility_identifier": "sppd_facility_identifier",
+            "ICFSourceID": "Source ID",
+            "y_coordinate" : "Latitude",
+            "x_coordinate" : "Longitude",
+            "ICFCatLevelModeling" : "ICFCatLevelModeling"
+        }
+        key_cols = list(columns.keys())
 
         # keep an eye out on cells that could be classified as NaN instead of ""
-        tmp = self.df[columns].drop_duplicates()
-        tmp = tmp.sort_values(columns)
+        tmp = self.df[key_cols].drop_duplicates()
+        tmp = tmp.sort_values(key_cols)
         tmp = tmp.loc[
             (tmp["y_coordinate"] != "")
             & (tmp["x_coordinate"] != "")
             & (tmp["ICFCatLevelModeling"] == "Yes")
         ]
 
-        columns.pop()  # drop ICFCatLevelModeling
-        self.working_MPLatLongs = tmp[columns]
-
-    # working_MPLatLongsLine
-    def qryMP03aa_LineLatLons01(self):
-        columns = [
-            "ICFFacilityID", # Facility ID
-            "sppd_facility_identifier",
-            "ICFSourceID", # Source ID
-            "fugitive_2d_midpoint1_y_coordinate", # LineLatitude
-            "fugitive_2d_midpoint1_x_coordinate", # LineLongitude
-            "ICFCatLevelModeling",
-            "ICFSourceType"
-        ]
-
-        tmp = self.df[columns].drop_duplicates()
-        tmp = tmp.sort_values(columns)
-        tmp = tmp.loc[
-            (tmp["ICFSourceType"] == "N")
-            & (tmp["ICFCatLevelModeling"] == "Yes")
-        ]
-
-        # drop ICFCatLevelModeling, ICFSourceType
-        columns = columns[:-2]
-        self.working_MPLatLongsLine = tmp[columns]
+        del columns["ICFCatLevelModeling"]
+        self.working_MPLatLongs = tmp[list(columns.keys())].rename(columns=columns)
 
     # working_MPLatLongsLine
     # TODO -- get working data for this
-    def qryMP03aa_LineLatLons02(self):
-        columns = [
-            "ICFFacilityID", # Facility ID
-            "sppd_facility_identifier",
-            "ICFSourceID", # Source ID
-            "fugitive_2d_midpoint2_y_coordinate", # LineLatitude
-            "fugitive_2d_midpoint2_x_coordinate", # LineLongitude
-            "ICFCatLevelModeling",
-            "ICFSourceType"
-        ]
+    def qryMP03aa_LineLatLons01(self):
+        columns = {
+            "ICFFacilityID": "Facility ID",
+            "sppd_facility_identifier": "sppd_facility_identifier",
+            "fugitive_2d_midpoint1_y_coordinate": "LineLatitude",
+            "fugitive_2d_midpoint1_x_coordinate": "LineLongitude",
+            "ICFCatLevelModeling": "ICFCatLevelModeling",
+            "ICFSourceType": "ICFSourceType",
+        }
+        key_cols = list(columns.keys())
 
-        tmp = self.df[columns].drop_duplicates()
-        tmp = tmp.sort_values(columns)
+        tmp = self.df[key_cols].drop_duplicates()
+        tmp = tmp.sort_values(key_cols)
         tmp = tmp.loc[
-            (tmp["ICFSourceType"] == "N")
-            & (tmp["ICFCatLevelModeling"] == "Yes")
+            (tmp["ICFSourceType"] == "N") & (tmp["ICFCatLevelModeling"] == "Yes")
         ]
 
-        # drop ICFCatLevelModeling, ICFSourceType
-        columns = columns[:-2]
-        # self.working_MPLatLongsLine = tmp[columns]
+        del columns["ICFCatLevelModeling"]
+        del columns["ICFSourceType"]
+        tmp = tmp[list(columns.keys())]
+
+        self.working_MPLatLongsLine = tmp.rename(columns=columns)
+
+    # working_MPLatLongsLine
+    def qryMP03aa_LineLatLons02(self):
+        columns = {
+            "ICFFacilityID": "Facility ID",
+            "sppd_facility_identifier": "sppd_facility_identifier",
+            "fugitive_2d_midpoint2_y_coordinate": "LineLatitude",
+            "fugitive_2d_midpoint2_x_coordinate": "LineLongitude",
+            "ICFCatLevelModeling": "ICFCatLevelModeling",
+            "ICFSourceType": "ICFSourceType",
+        }
+        key_cols = list(columns.keys())
+
+        tmp = self.df[key_cols].drop_duplicates()
+        tmp = tmp.sort_values(key_cols)
+        tmp = tmp.loc[
+            (tmp["ICFSourceType"] == "N") & (tmp["ICFCatLevelModeling"] == "Yes")
+        ]
+
+        del columns["ICFCatLevelModeling"]
+        del columns["ICFSourceType"]
+        tmp = tmp[list(columns.keys())]
+
+        tmp = tmp.rename(columns=columns)
+        self.working_MPLatLongsLine = pd.concat([self.working_MPLatLongsLine, tmp], ignore_index=True)
+
+    # working_MPLatLongs
+    # TODO -- get working data for this
+    """
+    Groups by facility id, sppd_facility_id, source id ... take the average of LineLatitude as latitude ... same with Longitude
+    in these groups, then concat into working_MPLatLongs
+    """
+    def qryMP03ab_AppendLineLatLongs(self):
+        columns = {
+            "ICFFacilityID" : "Facility ID",
+            "sppd_facility_identifier": "sppd_facility_identifier",
+            "ICFSourceID": "Source ID",
+            "y_coordinate" : "Latitude",
+            "x_coordinate" : "Longitude"
+        }
+        key_cols = list(columns.keys())
+
+    # working_MP04HH_T1ChemResults
+    def qryMP04aHH_CreateShellForChemSVs(self):
+        """
+        SELECT "" AS [Src Cat], 
+        qryMP03b_Avg_SourceLatLons_CatLevel.[Facility ID], 
+        qryMP03b_Avg_SourceLatLons_CatLevel.[Avg Lat] AS Lat, 
+        qryMP03b_Avg_SourceLatLons_CatLevel.[Avg Long] AS [Long], 
+        static_MP_PBHAPChems_withHHEquivalencyFactors.[ShortPB-HAP/EcoHAPName] AS [PB-HAP Grp], 
+        static_MP_PBHAPChems_withHHEquivalencyFactors.[Chem Name For Tier 2 Tool] AS Chem, 
+        CDbl(0) AS [Emiss (TPY; chem)], static_MP_PBHAPChems_withHHEquivalencyFactors.TEF AS [TEF (chem)], 
+        static_MP_PBHAPChems_withHHEquivalencyFactors.EEF AS [EEF (chem)], 
+        static_MP_PBHAPChems_withHHEquivalencyFactors.REF AS [REF (chem)], 
+        static_MP_PBHAPChems_withHHEquivalencyFactors.[Date PB-HAP REF Created] AS [Date REF Created], 
+        CDbl(0) AS [Emiss*REF (TPY; chem)], 
+        static_MP_HHScreeningThresholds.[Tier 1 Screening Threshold (TPY)] AS [Scrn Thresh (TPY; grp)], 
+        static_MP_HHScreeningThresholds.[Date Threshold Created] AS [Date Scrn Thresh Created], 
+        CDbl(0) AS [SV (chem)] INTO working_MP04HH_T1ChemResults
+
+        FROM qryMP03b_Avg_SourceLatLons_CatLevel, 
+        static_MP_PBHAPChems_withHHEquivalencyFactors 
+        INNER JOIN static_MP_HHScreeningThresholds 
+        ON static_MP_PBHAPChems_withHHEquivalencyFactors.[ShortPB-HAP/EcoHAPName] = static_MP_HHScreeningThresholds.[ShortPB-HAPName]
+
+        GROUP BY "", 
+        qryMP03b_Avg_SourceLatLons_CatLevel.[Facility ID], 
+        qryMP03b_Avg_SourceLatLons_CatLevel.[Avg Lat], 
+        qryMP03b_Avg_SourceLatLons_CatLevel.[Avg Long], 
+        static_MP_PBHAPChems_withHHEquivalencyFactors.[ShortPB-HAP/EcoHAPName], 
+        static_MP_PBHAPChems_withHHEquivalencyFactors.[Chem Name For Tier 2 Tool], 
+        static_MP_PBHAPChems_withHHEquivalencyFactors.TEF, 
+        static_MP_PBHAPChems_withHHEquivalencyFactors.EEF, 
+        static_MP_PBHAPChems_withHHEquivalencyFactors.REF, 
+        static_MP_PBHAPChems_withHHEquivalencyFactors.[Date PB-HAP REF Created], 
+        static_MP_HHScreeningThresholds.[Tier 1 Screening Threshold (TPY)], 
+        static_MP_HHScreeningThresholds.[Date Threshold Created], 
+        CDbl(0)
+
+        ORDER BY "", 
+        qryMP03b_Avg_SourceLatLons_CatLevel.[Facility ID], 
+        static_MP_PBHAPChems_withHHEquivalencyFactors.[ShortPB-HAP/EcoHAPName], 
+        static_MP_PBHAPChems_withHHEquivalencyFactors.[Chem Name For Tier 2 Tool];
+        """
+        pass
