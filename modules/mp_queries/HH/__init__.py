5767271583b24812e0e298ab7f5b81eadf481be1
Berkowitz <55586@icf.com>
2023-08-11 10:32:16 -0400

    integers that dont need to be dbls are not longer made dbls

--- modules/mp_queries/HH/__init__.py
+++ modules/mp_queries/HH/__init__.py
@@ -4,6 +4,7 @@ from modules.mp_queries.HH._03_summary_setup import SummarySetup
 from modules.mp_queries.HH._04_summary_gather import SummaryGather
 from modules.mp_queries.HH._05_summary_populate import SummaryPopulate
 
+from modules.utils import src_cat_name, emission_type
 
 def run_HH_module(working_crosswalk, latlons, accdb):
     HH_obj = Template(working_crosswalk, latlons)
@@ -11,6 +12,11 @@ def run_HH_module(working_crosswalk, latlons, accdb):
     SummarySetup(HH_obj)
     SummaryGather(HH_obj)
     SummaryPopulate(HH_obj)
+
+    src_cat_str = f"{src_cat_name}_{emission_type}"
+    HH_obj.working_MP04HH_T1ChemResults["Src Cat"] = src_cat_str
+    HH_obj.working_MP05HH_T1GrpResults["Src Cat"] = src_cat_str
+    HH_obj.working_MP07HH_T1Summary["Src Cat"] = src_cat_str
 
     accdb.write(
         "05 - T1 HumHealth Mpath Scrn By Facil-Chem",
