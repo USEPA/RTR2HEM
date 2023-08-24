4853e1f3288bba2494aae3b43c81e7c20f8c84d2
Berkowitz <55586@icf.com>
2023-08-24 16:09:30 -0400

    accdb and table import select

--- modules/GUI/settings.py
+++ modules/GUI/settings.py
@@ -14,48 +14,6 @@ class SettingsGUI(GUI):
         except Exception as e:
             self.error(e)
 
-    def disableChildren(self, parent):
-        for child in parent.winfo_children():
-            wtype = child.winfo_class()
-            if wtype not in ("Frame", "Labelframe"):
-                child.configure(state="disable")
-            else:
-                self.disableChildren(child)
-
-    def enableChildren(self, parent):
-        for child in parent.winfo_children():
-            wtype = child.winfo_class()
-            if wtype not in ("Frame", "Labelframe"):
-                child.configure(state="normal")
-            else:
-                self.enableChildren(child)
-
-    def import_file(self, subroot, gen):
-        working_dir = os.getcwd()
-        ftypes = [("Microsoft Access Database", "*.accdb"), ("Excel files", ".xlsx")]
-        filepath = filedialog.askopenfilename(initialdir=working_dir, filetypes=ftypes)
-
-        if pathlib.Path(filepath).suffix == ".accdb":
-            accdb_reader = AccdbHandle(filepath, how="open")
-            tables = accdb_reader.get_tables()
-
-            variable = StringVar(subroot)
-            variable.set(tables[0])  # default value
-            w = OptionMenu(subroot, variable, *tables)
-            w.grid(row=next(gen), column=0, columnspan=10)
-
-    def clear_all(self, is_checked, regCodes):
-        for regCode in regCodes:
-            regCode.set(0)
-        is_checked.set(0)
-
-    def select_all(self, is_checked, regCode_button_list):
-        for regCode in regCode_button_list:
-            if is_checked.get():
-                regCode.set(1)
-            else:
-                regCode.set(0)
-
     def main(self):
         regCode_button_list = []
 
@@ -94,12 +52,22 @@ class SettingsGUI(GUI):
         src_cat_name_textbox = Entry(subroot, width=50)
         src_cat_name_textbox.grid(row=self.current_gen, column=1, sticky=W)
 
+        ##############################################################
+        # accdb and table select
+
+        tables_var = StringVar(subroot)
+        tables_var.set("")  # default value
+        tables_menu = OptionMenu(subroot, tables_var, *[""])
+
         select_input_file_btn = Button(
             subroot,
-            text="Import a file",
-            command=lambda: self.import_file(subroot, gen),
+            text="Import File",
+            command=lambda: self.import_file(tables_menu, tables_var),
         )
         select_input_file_btn.grid(row=next(gen), sticky=W)
+        tables_menu.grid(row=next(gen), column=0, columnspan=10)
+
+        ##############################################################
 
         select_all_var = IntVar()
         select_all_btn = Checkbutton(
@@ -123,3 +91,48 @@ class SettingsGUI(GUI):
         )
 
         super().main()
+
+    def disableChildren(self, parent):
+        for child in parent.winfo_children():
+            wtype = child.winfo_class()
+            if wtype not in ("Frame", "Labelframe"):
+                child.configure(state="disable")
+            else:
+                self.disableChildren(child)
+
+    def enableChildren(self, parent):
+        for child in parent.winfo_children():
+            wtype = child.winfo_class()
+            if wtype not in ("Frame", "Labelframe"):
+                child.configure(state="normal")
+            else:
+                self.enableChildren(child)
+
+    def import_file(self, dropdown, var):
+        menu = dropdown["menu"]
+        menu.delete(0, "end")
+
+        working_dir = os.getcwd()
+        ftypes = [("Microsoft Access Database", "*.accdb"), ("Excel files", ".xlsx")]
+        filepath = filedialog.askopenfilename(initialdir=working_dir, filetypes=ftypes)
+
+        # TODO add handling for an excel file + sheet name as well
+        # if pathlib.Path(filepath).suffix == ".accdb":
+
+        accdb_reader = AccdbHandle(filepath, how="open")
+        tables = accdb_reader.get_tables()
+        for table in tables:
+            menu.add_command(label=table, command=lambda name=table: var.set(name))
+        var.set(tables[0])
+
+    def clear_all(self, is_checked, regCodes):
+        for regCode in regCodes:
+            regCode.set(0)
+        is_checked.set(0)
+
+    def select_all(self, is_checked, regCode_button_list):
+        for regCode in regCode_button_list:
+            if is_checked.get():
+                regCode.set(1)
+            else:
+                regCode.set(0)
